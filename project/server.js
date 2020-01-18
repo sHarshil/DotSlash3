4019a96ff330b31097fb663a461813095f085ca2
Harshil Shah <sharshil1299@gmail.com>
2020-01-18 16:44:05 +0530

    Template is Updating

--- project/server.js
+++ project/server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-constants = require("./backend/constants")
+constants = require("./backend/constants");
 
 var app = express();
 app.use(bodyParser.json());
@@ -16,29 +16,32 @@ app.use(express.static(distDir));
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
+mongodb.MongoClient.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/test",
+  function (err, client) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+
+    // Save database object from the callback for reuse.
+    db = client.db();
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+      var port = server.address().port;
+      console.log("App now running on port", port);
+    });
   }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+);
 
 // CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
-  res.status(code || 500).json({ "error": message });
+  res.status(code || 500).json({ error: message });
 }
 
 /*  "/api/contacts"
@@ -47,26 +50,29 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/api/actions", function (req, res) {
-  db.collection(constants.ACTIONS_COLLECTION).find({}).toArray(function (err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get actions.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
+  db.collection(constants.ACTIONS_COLLECTION)
+    .find({})
+    .toArray(function (err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get actions.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
 });
-
 
 app.post("/api/actions", function (req, res) {
   var bb = {
-    "name": req.body.name,
-    "createDate": new Date()
-  }
+    name: req.body.name,
+    createDate: new Date()
+  };
   if (!req.body.name) {
     handleError(res, "Invalid user input", "Must provide a name.", 400);
-  }
-  else {
-    db.collection(constants.ACTIONS_COLLECTION).insertOne(bb, function (err, doc) {
+  } else {
+    db.collection(constants.ACTIONS_COLLECTION).insertOne(bb, function (
+      err,
+      doc
+    ) {
       if (err) {
         handleError(res, err.message, "Failed to create new contact.");
       } else {
@@ -77,14 +83,15 @@ app.post("/api/actions", function (req, res) {
 });
 
 app.get("/api/action/:id", function (req, res) {
-  db.collection(constants.SUBACTIONS_COLLECTION).find({ "action_id": ObjectID(req.params.id) }).toArray(function (err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get actions.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
-
+  db.collection(constants.SUBACTIONS_COLLECTION)
+    .find({ action_id: ObjectID(req.params.id) })
+    .toArray(function (err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get actions.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
 });
 
 app.post("/api/action/:id", function (req, res) {
@@ -97,44 +104,47 @@ app.post("/api/action/:id", function (req, res) {
     handleError(res, "Invalid user input", "Must provide a name.", 400);
   }
   var bb = {
-    "name": req.body.name,
-    "action_id": ObjectID(req.params.id),
-    "createDate": new Date()
-  }
+    name: req.body.name,
+    action_id: ObjectID(req.params.id),
+    createDate: new Date()
+  };
 
-  db.collection(constants.SUBACTIONS_COLLECTION).insertOne(bb,
-    function (err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to create new Subaction.");
-      } else {
-        res.status(201).json(doc.ops[0]);
-      }
+  db.collection(constants.SUBACTIONS_COLLECTION).insertOne(bb, function (
+    err,
+    doc
+  ) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new Subaction.");
+    } else {
+      res.status(201).json(doc.ops[0]);
     }
-  );
+  });
 });
 
 app.get("/api/templates/:subaction", function (req, res) {
-  db.collection(constants.TEMPLATES_COLLECTION).find({ "subaction_id": ObjectID(req.params.subaction) }).toArray(function (err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get actions.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
-
+  db.collection(constants.TEMPLATES_COLLECTION)
+    .find({ subaction_id: ObjectID(req.params.subaction) })
+    .toArray(function (err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get actions.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
 });
 
 app.get("/api/template/:templateid", function (req, res) {
   console.log(req.params.templateid);
-  db.collection(constants.TEMPLATES_COLLECTION).find({ "_id": ObjectID(req.params.templateid) }).toArray(function (err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get templates.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
+  db.collection(constants.TEMPLATES_COLLECTION)
+    .find({ _id: ObjectID(req.params.templateid) })
+    .toArray(function (err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get templates.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
 });
-
 
 app.post("/api/templates/:subaction_id", function (req, res) {
   var newSubAction = req.body;
@@ -153,22 +163,23 @@ app.post("/api/templates/:subaction_id", function (req, res) {
   // ToDo: Check if subaction ID exists or not
 
   var bb = {
-    "subaction_id": ObjectID(req.params.subaction_id),
-    "language": req.body.name,
-    "template": req.body.template,
-    "instructions": req.params.instructions,
-    "createDate": new Date()
-  }
+    subaction_id: ObjectID(req.params.subaction_id),
+    language: req.body.name,
+    template: req.body.template,
+    instructions: req.params.instructions,
+    createDate: new Date()
+  };
 
-  db.collection(constants.TEMPLATES_COLLECTION).insertOne(bb,
-    function (err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to create new template.");
-      } else {
-        res.status(201).json(doc.ops[0]);
-      }
+  db.collection(constants.TEMPLATES_COLLECTION).insertOne(bb, function (
+    err,
+    doc
+  ) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new template.");
+    } else {
+      res.status(201).json(doc.ops[0]);
     }
-  );
+  });
 });
 
 /*  "/api/contacts/:id"
@@ -187,47 +198,70 @@ app.post("/api/templates/:subaction_id", function (req, res) {
 //   });
 // });
 
-// app.put("/api/contacts/:id", function (req, res) {
-//   var updateDoc = req.body;
-//   delete updateDoc._id;
+app.put("/api/action/:id", function (req, res) {
+  var updateDoc = { $set: req.body }; // { $set: { name: "Hi there" } };
 
-//   db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
-//     if (err) {
-//       handleError(res, err.message, "Failed to update contact");
-//     } else {
-//       updateDoc._id = req.params.id;
-//       res.status(200).json(updateDoc);
-//     }
-//   });
-// });
+  delete updateDoc._id;
+  //console.log(req.params.id);
 
-app.delete("/api/action/:id", function (req, res) {
-  db.collection(constants.ACTIONS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete contact");
-    } else {
-      res.status(200).json(req.params.id);
+  db.collection(constants.ACTIONS_COLLECTION).updateOne(
+    { _id: new ObjectID(req.params.id) },
+    updateDoc,
+    function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update action");
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
     }
-  });
+  );
 });
 
-// app.delete("/api/action/:id", function (req, res) {
-//   db.collection(constants.ACTIONS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
-//     if (err) {
-//       handleError(res, err.message, "Failed to delete contact");
-//     } else {
-//       res.status(200).json(req.params.id);
-//     }
-//   });
-// });
 
+
+app.put("/api/template/:id", function (req, res) {
+  var updateDoc = { $set: req.body }; // { $set: { name: "Hi there" } };
+
+  delete updateDoc._id;
+  //console.log(req.params.id);
+
+  db.collection(constants.TEMPLATES_COLLECTION).updateOne(
+    { _id: new ObjectID(req.params.id) },
+    updateDoc,
+    function (err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update Template");
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
+    }
+  );
+});
+
+app.delete("/api/action/:id", function (req, res) {
+  db.collection(constants.ACTIONS_COLLECTION).deleteOne(
+    { _id: new ObjectID(req.params.id) },
+    function (err, result) {
+      if (err) {
+        handleError(res, err.message, "Failed to delete contact");
+      } else {
+        res.status(200).json(req.params.id);
+      }
+    }
+  );
+});
 
 app.delete("/api/template/:templateid", function (req, res) {
-  db.collection(constants.TEMPLATES_COLLECTION).deleteOne({ _id: new ObjectID(req.params.templateid) }, function (err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete contact");
-    } else {
-      res.status(200).json(req.params.templateid);
+  db.collection(constants.TEMPLATES_COLLECTION).deleteOne(
+    { _id: new ObjectID(req.params.templateid) },
+    function (err, result) {
+      if (err) {
+        handleError(res, err.message, "Failed to delete contact");
+      } else {
+        res.status(200).json(req.params.templateid);
+      }
     }
-  });
+  );
 });
