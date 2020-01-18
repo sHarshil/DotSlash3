53f3458da3c23350efa9c0d8d8430c15e7aced2c
Harshil Shah <sharshil1299@gmail.com>
2020-01-18 19:02:57 +0530

    Monaco Editor

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './components/welcome.component';
-import { CodingComponent } from './components/code/coding.component';
+import { CodingAreaComponent } from "./components/coding-area/coding-area.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { WelcomeComponent } from "./components/welcome.component";
+import { CodingComponent } from "./components/code/coding.component";
 
-
-const routes: Routes = [  
-  { 
-    path: '', 
-    component: WelcomeComponent 
+const routes: Routes = [
+  {
+    path: "",
+    component: WelcomeComponent
   },
-  { 
-    path: 'code', 
-    component: CodingComponent 
+  {
+    path: "code",
+    component: CodingComponent
   },
+  {
+    path: "codearea",
+    component: CodingAreaComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,23 +1,24 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef } from "@angular/core";
 import { ApplicationStateService } from "./services/application-state.service";
 import { MediaMatcher } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'project';
+  title = "project";
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
 
   constructor(
-    media: MediaMatcher,changeDetectorRef: ChangeDetectorRef, public applicationStateService: ApplicationStateService)
-  {
+    media: MediaMatcher,
+    changeDetectorRef: ChangeDetectorRef,
+    public applicationStateService: ApplicationStateService
+  ) {
     this.mobileQuery = media.matchMedia("(max-width: 600px)");
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-
   }
 }
