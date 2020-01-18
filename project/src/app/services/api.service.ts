17d2556ae545a7ec9f895e5c359f006ddbc044e4
Yash Sodha <yash.sodha@gmail.com>
2020-01-18 21:03:01 +0530

    no message

--- project/src/app/services/api.service.ts
+++ project/src/app/services/api.service.ts
@@ -71,5 +71,67 @@ export class APIService {
           );
     }
 
+    public makeTemplate(sub_action_id, success, failure): void 
+    {
+        var data = {};
+        this.httpClient
+          .post(environment.backend_url + "/templates/" + sub_action_id, data, {
+            responseType: "text",
+            headers: { "Content-Type": "application/json; charset=utf-8" }
+          })
+          .subscribe(
+            res => {
+              const response = JSON.parse(res);
+              console.log(response);
+              success(response);
+            },
+            err => {
+              console.log(err);
+              failure(err);
+            }
+        );
+    }
 
+    public makeSubAction(action_id, success, failure): void 
+    {
+        var data = {};
+        this.httpClient
+          .post(environment.backend_url + "/action/" + action_id, data, {
+            responseType: "text",
+            headers: { "Content-Type": "application/json; charset=utf-8" }
+          })
+          .subscribe(
+            res => {
+              const response = JSON.parse(res);
+              console.log(response);
+              success(response);
+            },
+            err => {
+              console.log(err);
+              failure(err);
+            }
+        );
+    }
+    public makeAction(action_name, success, failure): void 
+    {
+        var data = {
+            "name": action_name
+        };
+        this.httpClient
+          .post(environment.backend_url + "/actions", data, {
+            responseType: "text",
+            headers: { "Content-Type": "application/json; charset=utf-8" }
+          })
+          .subscribe(
+            res => {
+              const response = JSON.parse(res);
+              console.log(response);
+              success(response);
+            },
+            err => {
+              console.log(err);
+              failure(err);
+            }
+        );
+    }
 }
