99bd7caae052f692afee89669f2f0aee273bf361
Mitkumar Patel <mitpatel23897@gmail.com>
2020-01-19 02:36:13 +0530

    Copy btn added

--- project/src/app/components/code/coding.component.ts
+++ project/src/app/components/code/coding.component.ts
@@ -138,9 +138,19 @@ export class CodingComponent implements OnInit {
       }
     );
   }
-  copy() {
-    //alert(this.code);
-  }
+  copy(val: string) {
+      let selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = val;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        document.execCommand('copy');
+        document.body.removeChild(selBox);
+      }
   /*
     Coding-Area
     */
