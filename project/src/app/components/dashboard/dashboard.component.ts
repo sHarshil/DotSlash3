725464aede77ecaa136b648ad4abe182e84a8055
Yash Sodha <yash.sodha@gmail.com>
2020-01-19 04:23:20 +0530

    Copy added

--- project/src/app/components/dashboard/dashboard.component.ts
+++ project/src/app/components/dashboard/dashboard.component.ts
@@ -387,7 +387,23 @@ deleteTemplate(action)
       });
 }
 
+copyText(val: string){
+  let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
   copy() {
+    this.copyText(this.current_language["template"]);
+    this.showSnackBar("Copied to clipboard");
     //alert(this.code);
   }
   /*
